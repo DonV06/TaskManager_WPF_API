cab59245175b2ecc727b4acd64a0058a34e42023
DonV06 <77447076+Dawid7040@users.noreply.github.com>
2023-10-09 22:54:15 +0200

    sdg

--- API/views.py
+++ API/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 class DownloadPublicKeyView(View):
+    @csrf_exempt
     def get(self, request, *args, **kwargs):
         with open('publickey.pem', 'rb') as f:
             response = HttpResponse(f.read(), content_type='application/x-pem-file')
             response['Content-Disposition'] = 'inline;filename=publickey.pem'
         return response
-
-class APIRequestView(View):
-    def post(self, request, *args, **kwargs):
-        # Handle API requests here
-        return HttpResponse('API Request received')
+@csrf_exempt
+def APIRequest(request):
+    # Handle API requests here
+    return HttpResponse('API Request received')
