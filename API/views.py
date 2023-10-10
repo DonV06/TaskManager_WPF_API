eb5df50172fb4096da1a07a52ce691f3612c6097
DonV06 <77447076+Dawid7040@users.noreply.github.com>
2023-10-10 21:37:49 +0200

    After Rename

--- API/views.py
+++ API/views.py
@@ -1,17 +1,13 @@
-from cryptography.hazmat.backends.openssl import ciphers
-from cryptography.hazmat.primitives.ciphers import algorithms, modes
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
 from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-import json
-
-import base64
-from cryptography.hazmat.primitives.ciphers import Cipher
-from cryptography.hazmat.primitives import serialization, asymmetric, hashes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import padding
-
+import base64, json, uuid
 from API import models
 
 
@@ -25,14 +21,13 @@ class DownloadPublicKeyView(View):
 @csrf_exempt
 def APIRequest(request):
     if request.method == 'POST':
-        # Load the JSON data from the request body
-        data = json.loads(request.body.decode('utf-8'))
-        login = base64.b64decode(data["User"])
-        password = base64.b64decode(data["Password"])
-        AesKey = data["aeskey"].split("|")[0]
-        AesKeyIV = data["aeskey"].split("|")[1]
 
-        # Load the private key from a PEM file
+        SendedJson = json.loads(request.body.decode('utf-8'))
+        LoginEncrypted = base64.b64decode(SendedJson["User"])
+        PasswordEncrypted = base64.b64decode(SendedJson["Password"])
+        AesKeyEncrypted = SendedJson["aeskey"].split("|")[0]
+        AesKeyIVEncrypted = SendedJson["aeskey"].split("|")[1]
+
         with open('privatekey.pem', 'rb') as key_file:
             private_key = serialization.load_pem_private_key(
                 key_file.read(),
@@ -40,41 +35,25 @@ def APIRequest(request):
                 backend=default_backend()
             )
 
-        # Base64-encoded ciphertext from C#
-        AesKeyIV = base64.b64decode(AesKeyIV)
-        # Decode the base64 ciphertext to bytes
-        ciphertext = base64.b64decode(AesKey)
-        print(ciphertext)
+        AesKeyIVRsaOnly = base64.b64decode(AesKeyIVEncrypted)
+        AesKeyRsaOnly = base64.b64decode(AesKeyEncrypted)
         # Decrypt the ciphertext
-        plaintext = private_key.decrypt(
-            ciphertext,
-            padding.PKCS1v15()
-        )
-
-        AesKeyIV = private_key.decrypt(AesKeyIV, padding.PKCS1v15())
-        print(AesKeyIV)
-        # Convert the decrypted bytes to string (assuming the plaintext was UTF-8 encoded)
-        cipher = Cipher(
-            algorithms.AES(plaintext),
-            modes.CBC(AesKeyIV),
-            backend=default_backend()
-        )
-        decryptor = cipher.decryptor()
-        login = decryptor.update(login)
-        decryptor = cipher.decryptor()
-        password = decryptor.update(password)
-        print(login.decode().strip())
+        AesKey = private_key.decrypt(AesKeyRsaOnly,padding.PKCS1v15())
+        AesKeyIV = private_key.decrypt(AesKeyIVEncrypted, padding.PKCS1v15())
+        cipher = Cipher(algorithms.AES(AesKey),modes.CBC(AesKeyIVEncrypted),backend=default_backend())
+        Decryptor = cipher.decryptor()
+        LoginDecrypted = Decryptor.update(LoginEncrypted)
+        Decryptor = cipher.decryptor()
+        PasswordDecrypted = Decryptor.update(PasswordEncrypted)
         try:
-            userselected = models.User.objects.get(login=login.decode().strip())
+            userselected = models.User.objects.get(login=LoginDecrypted.decode().strip())
         except models.User.DoesNotExist:
             return HttpResponse(f"Account don't exists!")
 
-        if (userselected.password != password.decode().strip()): return HttpResponse("Password not correct!")
-        userselected.AESkey = base64.b64encode(plaintext)
-        import uuid
+        if (userselected.password != PasswordDecrypted.decode().strip()): return HttpResponse("Password not correct!")
+        userselected.AESkey = base64.b64encode(AesKey)
         useruuid = uuid.uuid4()
         userselected.token = useruuid
         userselected.save()
         return HttpResponse(f"Token: '{useruuid}'")
-    # Handle API requests here
     return HttpResponse('API Request received')
